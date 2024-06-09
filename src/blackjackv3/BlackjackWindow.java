d2b10e0560e847bbfdddea0331bf18761db73d99
CobyJM <vth7931@autuni.ac.nz>
2024-06-09 20:28:43 +1200

    tidying

--- src/blackjackv3/BlackjackWindow.java
+++ src/blackjackv3/BlackjackWindow.java
@@ -6,13 +6,11 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-//import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JLabel;
-//import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
